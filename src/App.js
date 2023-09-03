7b0ca4816dbb316b7def97751fef0baaf96672ae
Thomson T Kakkanattu <thomsontk237@gmail.com>
2023-09-03 14:12:19 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
+// import SearchBar from './components/SearchBar';
+import Search from './components/Search';
+import Navbar from './components/Navabar';
+import HistoryPage from "./components/HistoryPage"
+import WordDetailsPage from "./WordDetailsPage"
+
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navbar/>}>
+          <Route path="" element={<Search/>}/>
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/word/:word" element={<WordDetailsPage />} />
+          </Route>
+          
+          
+        </Routes>
+
+        {/* <Navbar/> */}
+      </div>
+    
   );
 }
 
